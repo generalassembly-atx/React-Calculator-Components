910cb53a9dfc1ea20c69b2226d1b6bfaace54359
Andrew Chan <accsuperstar@gmail.com>
2017-04-18 08:28:40 -0500

    react hw submit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Keys from './components/Keys';
+import Display from './components/Display';
+import Title from './components/Title';
 
 class App extends Component {
   render() {
@@ -7,48 +10,17 @@ class App extends Component {
       <div className="App">
         <h1>React Calculator!</h1>
         <div className="box">
-          <div className="title">
-            <p>GA Instruments TI-WDI</p>
-          </div>
-          <div className="display">
-            <input type="text" readOnly size="18" id="d"/>
-          </div>
-          <div className="keys">
-            <p>
-              <input type="button" className="button gray" value="mrc" />
-              <input type="button" className="button gray" value="m-" />
-              <input type="button" className="button gray" value="m+" />
-              <input type="button" className="button pink" value="/" />
-            </p>
-            <p>
-            <input type="button" className="button black" value="7" />
-              <input type="button" className="button black" value="8" />
-                <input type="button" className="button black" value="9" />
-                <input type="button" className="button pink" value="*" />
-              </p>
-              <p>
-                <input type="button" className="button black" value="4" />
-                <input type="button" className="button black" value="5" />
-                  <input type="button" className="button black" value="6" />
-                  <input type="button" className="button pink" value="-" />
-                  </p>
-                <p>
-                  <input type="button" className="button black" value="1" />
-                <input type="button" className="button black" value="2" />
-                  <input type="button" className="button black" value="3" />
-                  <input type="button" className="button pink" value="+" />
 
-                  </p>
-                <p>
-                  <input type="button" className="button black" value="0" />
-                <input type="button" className="button black" value="." />
-                  <input type="button" className="button black" value="C" />
-                  <input type="button" className="button orange" value="=" />
+            <Title />
 
-                  </p>
-              </div>
-            </div>
+          <div className="App-display">
+            <Display />
           </div>
+
+            <Keys />
+
+        </div>
+      </div>
     );
   }
 }
